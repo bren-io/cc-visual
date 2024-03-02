487e4caed50ba384f61ec659813a9bfdfa62e934
Zachery Linscott <45150570+zweatshirt@users.noreply.github.com>
2024-03-02 10:47:37 -0600

    replace pyqt req with tkinter

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
-
 import os
 import sys
 import moderngl as gl
-import PyQt5 as qt
 
-# This is the main file that will be ran. Everything else should ideally be put in a module and imported.
+
+# This is the main file that will be ran. 
+# Everything else should ideally be put in a module and imported.
+
+num = 17
+
+def collatz(n: int):
+    print(n)
+    if n == 1:
+        return n
+    elif n % 2 == 0:
+        collatz(n // 2)
+    else:
+        collatz(3*n + 1)
+
 
 def main():
     print("Hello world")
+    collatz(num)
+
+
 
 if __name__ == "__main__":
     main()
-
